f03aa92c43d48e532b77b770f31bc65cd65652de
Daniel Andrade <danielandradecarmo@gmail.com>
2023-11-20 14:54:24 -0300

    Converting the notebook to a python3 file

--- main.py
+++ main.py
@@ -1,44 +1,35 @@
-
 import pandas as pd
 import numpy as np
 
 #receive files as args
 import sys
 ratings_file = sys.argv[1]
-targets_file = sys.argv[2]
+content_file = sys.argv[2]
+targets_file = sys.argv[3]
 
-#read ratings.csv   
-df = pd.read_csv(ratings_file)
 
-#separate UserId:ItemId into two columns
-df[['UserId','ItemId']] = df['UserId:ItemId'].str.split(':',expand=True)
+#read ratings.csv
+df_ratings = pd.read_json(ratings_file, lines=True)
+df_ratings.drop(columns =["Timestamp"], inplace = True)
 
-#drop the original column 
-df.drop(columns =["UserId:ItemId"], inplace = True)
+#separate and vectorize unique users and items
+unique_users = df_ratings['UserId'].unique()
+unique_items = df_ratings['ItemId'].unique()
 
-#unique users and items,
-unique_users = df['UserId'].unique()
-unique_items = df['ItemId'].unique()
+ratings = df_ratings['Rating'].values
 
-#map users and items to indices
 user_to_index = {user: i for i, user in enumerate(unique_users)}
 item_to_index = {item: i for i, item in enumerate(unique_items)}
 
-#vectorize unique users and items
-user_indices = df['UserId'].map(user_to_index).values
-item_indices = df['ItemId'].map(item_to_index).values   
+user_indices = df_ratings['UserId'].map(user_to_index).values
+item_indices = df_ratings['ItemId'].map(item_to_index).values 
 
-#save ratings in a vector
-ratings = (df['Rating'].values)
-
-
+#we will use the same matrix factorization used in RC1, which is Funk SVD with bias terms
 #hyperparameters
-learning_rate = 0.0095 #Learning rate
-num_epochs = 19        #Number of epochs
-num_factors = 15       #Number of latent factors
-lambda_l2 = 0.095      #Regularization parameter for L2
-lambda_l1 = 0.01       #Regularization parameter for L1
-
+learning_rate = 0.007 #Learning rate
+num_epochs = 20        #Number of epochs
+num_factors = 30       #Number of latent factors
+regularization = 0.2   #Regularization parameter for user and item biases
 
 #number of unique users and items
 num_users = len(unique_users)
@@ -57,7 +48,7 @@ user_bias = np.zeros(num_users)
 item_bias = np.zeros(num_items)
 
 #batch size 
-batch_size = 400 
+batch_size = 256
 
 #number of batches
 batch_num = int(len(ratings) / batch_size) 
@@ -93,64 +84,102 @@ for epoch in range(num_epochs):
         error = rating_batch - rating_hat
 
         #calculate the gradients for the user and item biases, by multiplying the learning rate by the error, adding the regularization terms and the biases
-        user_bias[user_batch] += learning_rate * (error - lambda_l2 * user_bias[user_batch] - lambda_l1 * np.sign(user_bias[user_batch]))
-        item_bias[item_batch] += learning_rate * (error - lambda_l2 * item_bias[item_batch] - lambda_l1 * np.sign(item_bias[item_batch]))
+        user_bias[user_batch] += learning_rate * (error - regularization * user_bias[user_batch])
+        item_bias[item_batch] += learning_rate * (error - regularization * item_bias[item_batch])
 
-        #calculate the gradients for the user and item matrices, by multiplying the learning rate by the error, adding the regularization terms and the matrices
-        user_matrix[user_batch, :] += learning_rate * (error[:, np.newaxis] * item_matrix[item_batch, :] - lambda_l2 * user_matrix[user_batch, :] - lambda_l1 * np.sign(user_matrix[user_batch, :]))
-        item_matrix[item_batch, :] += learning_rate * (error[:, np.newaxis] * user_matrix[user_batch, :] - lambda_l2 * item_matrix[item_batch, :] - lambda_l1 * np.sign(item_matrix[item_batch, :]))
-
-
-    mse = np.mean(error ** 2)
-
-    #used for testing and improving the results
-    #print(f'Epoch {epoch + 1}/{num_epochs} | MSE: {mse:.5f}')
+        #update user and item latent feature matrices for each batch
+        user_matrix[user_batch, :] += learning_rate * (error[:, np.newaxis] * user_matrix[user_batch, :] - regularization * user_matrix[user_batch, :])
+        item_matrix[item_batch, :] += learning_rate * (error[:, np.newaxis] * item_matrix[item_batch, :] - regularization * item_matrix[item_batch, :])
 
 
-
-
-#read targets.csv
+#read targets and content files
 df_targets = pd.read_csv(targets_file)
+df_content = pd.read_json(content_file, lines=True)
 
-#separate UserId:ItemId into two columns
-df_targets[['User_id','Item_id']] = df_targets['UserId:ItemId'].str.split(':',expand=True)
+df_content['imdbRating'] = df_content['imdbRating'].replace('N/A', np.nan)
+df_content['imdbRating'] = df_content['imdbRating'].astype(float)
+df_content['imdbRating'].fillna(df_content['imdbRating'].mean(), inplace=True)
 
-#drop the original column
-df_targets.drop(columns =["UserId:ItemId"], inplace = True)
+#converting imdbRating to dictionary
+imdb_rating_dict = pd.Series(df_content.imdbRating.values, index=df_content.ItemId).to_dict()
 
-#function to round the predicted rating to the nearest integer if the decimal part is greater than or equal to 0.95
-def custom_round(value):
-    decimal_part = value - int(value)
-    if decimal_part >= 0.95:
-        return int(value) + 1
+#processing imdbVotes
+df_content['imdbVotes'] = df_content['imdbVotes'].replace('N/A', np.nan)
+df_content['imdbVotes'] = df_content['imdbVotes'].str.replace(',', '').astype(float)
+df_content['imdbVotes'].fillna(df_content['imdbVotes'].mean(), inplace=True)
+
+#converting imdbVotes to dictionary
+imdb_votes_dict = pd.Series(df_content.imdbVotes.values, index=df_content.ItemId).to_dict()
+
+df_targets = pd.merge(df_targets, pd.DataFrame(list(imdb_rating_dict.items()), columns=['ItemId', 'imdbRating']), on='ItemId')
+df_targets = pd.merge(df_targets, pd.DataFrame(list(imdb_votes_dict.items()), columns=['ItemId', 'imdbVotes']), on='ItemId')
+
+
+#normalizing the ratings to a 10 point scale
+# Função para normalizar os ratings para uma escala de 0 a 10
+def normalize_rating(value):
+    if '/' in value:
+        numerator, denominator = value.split('/')
+        return float(numerator) / float(denominator) * 10
+    elif '%' in value:
+        return float(value.strip('%')) / 10
     else:
-        return value
+        return None
 
+#function to calculate the average rating of a row
+def calculate_average_ratings(ratings):
+    normalized_ratings = []
+    for rating in ratings:
+        normalized_value = normalize_rating(rating['Value'])
+        if normalized_value is not None:
+            normalized_ratings.append(normalized_value)
 
-print('UserId:ItemId,Rating')
+    if normalized_ratings:
+        return sum(normalized_ratings) / len(normalized_ratings)
+    else:
+        return None
+
+#apply the function to the ratings column
+ratingsEnsemble = df_content[['ItemId', 'Ratings']].copy()
+ratingsEnsemble['AverageRating'] = ratingsEnsemble['Ratings'].apply(calculate_average_ratings)
+
+# replace null values with the mean of the non-null values
+ratingsEnsemble['AverageRating'].fillna(ratingsEnsemble['AverageRating'].mean(), inplace=True)
+ratingsEnsemble.drop(columns =["Ratings"], inplace = True)
+df_targets = pd.merge(df_targets, ratingsEnsemble, on='ItemId')
+
+#calculating predicted ratings
+#create new column fro the rating predictions
+df_targets['Rating'] = 0
 
 
 #for each pair (user,item) in the targets.csv file, predict the rating and print it
 for _, row in df_targets.iterrows():
 
     #get the user and item indices
-    user_index = user_to_index[row['User_id']]
-    item_index = item_to_index[row['Item_id']]
-    
-    #predict the rating by taking the dot product of the user and item vectors and adding the user and item biases and the ratings mean
-    predicted_rating = np.dot(user_matrix[user_index, :], item_matrix[item_index, :].T) + user_bias[user_index] + item_bias[item_index] + ratings_mean
-    
-    #if the predicted rating is greater than 5, set it to 5
-    if predicted_rating > 5:
-        predicted_rating = 5
+    user_index = user_to_index[row['UserId']]
 
-    #if the predicted rating is less than 1, set it to 1
-    if predicted_rating < 1:
-        predicted_rating = 1
-
-    print(f'{row["User_id"]}:{row["Item_id"]},{custom_round(predicted_rating)}')
+    if  row['ItemId'] in item_to_index:
+        item_index = item_to_index[row['ItemId']]
     
+        svd_rating = (np.dot(user_matrix[user_index, :], item_matrix[item_index, :].T) + user_bias[user_index] + item_bias[item_index] + ratings_mean) 
 
+        predicted_rating = svd_rating * row['imdbRating'] * row['imdbVotes'] * row['AverageRating'] 
+              
     
+    else:
+        svd_rating = ratings_mean
+        predicted_rating = svd_rating * row['imdbRating'] * row['imdbVotes'] * row['AverageRating'] 
+   
+    df_targets.at[_, 'Rating'] = predicted_rating
 
-    
+#by UserId, sort the predicted ratings in descending order
+df_targets.sort_values(by=['UserId', 'Rating'], ascending=[True, False], inplace=True)
+
+#format and print the output
+df_targets.drop(columns =["Rating", 'imdbRating', 'imdbVotes', 'AverageRating'], inplace = True)
+
+#print the output that would be this csv: df_targets.to_csv('submission.csv', index=False)
+print('UserId,ItemId')
+for _, row in df_targets.iterrows():
+    print(f'{row["UserId"]},{row["ItemId"]}')
